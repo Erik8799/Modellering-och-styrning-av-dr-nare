aafafad9d6ade107e423b6bf3fd38c1343a82441
Erik8799 <erik@nordis.se>
2025-05-16 17:04:52 +0200

    correction

---
+++ Modellering_och_styrning_av_dronare_Bilagor/Ry.m
@@ -0,0 +1,5 @@
+function T = Ry(theta)
+  T = [cos(theta) 0 -sin(theta) ; 
+       0          1  0          ; 
+       sin(theta) 0  cos(theta)];
+end
---
+++ Modellering_och_styrning_av_dronare_Bilagor/Rz.m
@@ -0,0 +1,5 @@
+function T = Rz(Psi)
+T = [ cos(Psi) sin(Psi) 0 ; 
+     -sin(Psi) cos(Psi) 0 ; 
+      0        0        1];
+end
